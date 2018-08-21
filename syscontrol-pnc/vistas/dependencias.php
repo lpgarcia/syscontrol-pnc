3f51d01c4e05da41a84760ce7a845a880be14cf8
Jdeleons <jdeleons3@miumg.edu.gt>
2018-08-20 23:12:43 -0600

    agregando dependencias

---
+++ syscontrol-pnc/vistas/dependencias.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" href="../librerias/alertifyjs/css/alertify.css">
+<link rel="stylesheet" href="../librerias/alertifyjs/css/themes/default.css">
+<link rel="stylesheet" href="../librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="../librerias/select2/css/select2.css">
+
+
+<script src="../librerias/jquery-3.2.1.min.js" charset="utf-8"></script>
+<script src="../librerias/alertifyjs/alertify.js" charset="utf-8"></script>
+<script src="../librerias/bootstrap/js/bootstrap.js" charset="utf-8"></script>
+<script src="../librerias/select2/js/select2.js" charset="utf-8"></script>
---
+++ syscontrol-pnc/vistas/menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Syscontrol-PNC</title>
+		<?php
+			require_once "dependencias.php";
+		 ?>
+	</head>
+	<body>
+
+	</body>
+</html>
